bc853d9dbf99e968f7b164aa6d01b591f2ac6210
Joe Ybarra <joe@thinkbean.com>
2016-09-16 01:22:36 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './assets')
+]
+
+module.exports = {
+  context: __dirname,
+  entry: './assets/js/App.jsx',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.scss'],
+    root: [path.join(__dirname, './assets')]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      }
+    ]
+  }
+}
